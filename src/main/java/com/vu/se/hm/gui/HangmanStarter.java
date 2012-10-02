e2154a1b8385a539fe4987613125615fdb07ae0e
Dan <Dan@Dan-Laptop>
2012-10-02 00:42:13 -0400

    Connected logic with view. Still have to connect starter screen.

--- src/main/java/com/vu/se/hm/gui/HangmanStarter.java
+++ src/main/java/com/vu/se/hm/gui/HangmanStarter.java
@@ -4,6 +4,10 @@ public class HangmanStarter {
 
 	public static void main(String[] sd) {
 		String word = "test";
+              /*  HangmanWelcomeScreen starter = new HangmanWelcomeScreen();
+                starter.setSize(800,600);
+                starter.setVisible(true);0*/
+                
 		HangmanViewController hangmanViewController = new HangmanViewController(word);
 	}
 
